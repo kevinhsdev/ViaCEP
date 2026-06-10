fa37f31b11c2031cf22d6568bdc8e805838e6fa6
kevinhsdev <kevinhs07@outlook.com>
2026-06-10 00:35:47 -0300

    finaliza projeto adicionando API + codigo na main

--- src/ConsultaCEP.java
+++ src/ConsultaCEP.java
@@ -1,3 +1,6 @@
+import com.google.gson.Gson;
+
+import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -7,17 +10,22 @@ public class ConsultaCEP {
 
 
     public Endereco buscaEndereco(String cep) {
-        URI endereco = "https://viacep.com.br/ws/" + cep + "/json/";
+        URI endereco = URI.create("https://viacep.com.br/ws/" + cep + "/json/");
 
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(endereco))
+                .uri(endereco)
                 .build();
 
-        HttpResponse<String> response = HttpClient
-                .newHttpClient()
-                .send(request, HttpResponse.BodyHandlers.ofString());
+        HttpResponse<String> response = null;
+        try {
+            response = HttpClient
+                    .newHttpClient()
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException("Não consegui obter o endereço a partir desse CEP... " + e);
+        }
 
-        return new Gson.fromJson(response.body(), Endereco.class);
+        return new Gson().fromJson(response.body(), Endereco.class);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
-
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        IO.println("Digite o CEP desejado aqui: ");
+        String CEP = input.nextLine();
+        ConsultaCEP consultaCep = new ConsultaCEP();
+        Endereco endereco = consultaCep.buscaEndereco(CEP); // Seu CEP aqui...
+        System.out.println(endereco);
 
     }
 }
