6b5b6225699d7ff59ff2032ce96a3c4d8780221b
kevinhsdev <kevinhs07@outlook.com>
2026-06-09 23:48:18 -0300

    first commit

---
+++ src/Endereco.java
@@ -0,0 +1,6 @@
+public record Endereco(String cep,
+                       String logradouro,
+                       String complemento,
+                       String localidade,
+                       String uf) {
+}
